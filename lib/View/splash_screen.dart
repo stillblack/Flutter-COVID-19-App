22ddd7849104673e9dc10258f7955a822ea31fef
StillBlack <stillblack2014@gmail.com>
2026-05-20 20:53:46 +0500

    splash screen is  created

--- lib/View/splash_screen.dart
+++ lib/View/splash_screen.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:covid_tracker/View/world_state.dart';
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -8,16 +11,22 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin{
-
-
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(duration: const Duration(seconds: 3), vsync: this)
+        ..repeat();
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
-
+    Timer(const Duration(seconds: 5), () {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const WorldStateScreen()));
+    });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,8 +34,29 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children:  [
-
+          children: [
+            AnimatedBuilder(
+                animation: _controller,
+                child: Container(
+                    height: 200,
+                    width: 200,
+                    child: const Center(
+                        child: Image(
+                            image: AssetImage('assets/images/virus.png')))),
+                builder: (BuildContext context, Widget? child) {
+                  return Transform.rotate(
+                    angle: _controller.value * 2 * math.pi,
+                    child: child,
+                  );
+                }),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.08),
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Covid-19\n Tracker App',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+            ),
           ],
         ),
       ),
---
+++ lib/View/world_state.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WorldStateScreen extends StatefulWidget {
+  const WorldStateScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WorldStateScreen> createState() => _WorldStateScreenState();
+}
+
+class _WorldStateScreenState extends State<WorldStateScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
