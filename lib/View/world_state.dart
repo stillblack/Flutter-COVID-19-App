7ff59dd04ffa0bbe8b889b800600139267c75658
StillBlack <stillblack2014@gmail.com>
2026-05-23 03:49:15 +0500

    pie chart and ui is completed

--- lib/View/world_state.dart
+++ lib/View/world_state.dart
@@ -29,24 +29,77 @@ class _WorldStateScreenState extends State<WorldStateScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * .01,
-            ),
-            PieChart(
-              dataMap: const {
-                "Total": 20,
-                "Recoverd": 15,
-                "Deaths": 5,
-              },
-              animationDuration: const Duration(milliseconds: 1200),
-              chartType: ChartType.ring,
-              colorList: colorList,
-            ),
-          ]),
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .01,
+              ),
+              PieChart(
+                dataMap: const {
+                  "Total": 20,
+                  "Recoverd": 15,
+                  "Deaths": 5,
+                },
+                chartRadius: MediaQuery.of(context).size.width / 3.2,
+                legendOptions:
+                    const LegendOptions(legendPosition: LegendPosition.left),
+                animationDuration: const Duration(milliseconds: 1200),
+                chartType: ChartType.ring,
+                // colorList: colorList,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    vertical: MediaQuery.of(context).size.height * 0.06),
+                child: const Card(
+                  child: Column(
+                    children: [
+                      ReuseAbleRow(title: "Total", value: '200'),
+                      ReuseAbleRow(title: "Recoverd", value: "15"),
+                      ReuseAbleRow(title: "Deaths", value: "5")
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(10)),
+                child:
+                    const Center(child: Center(child: Text('Track Countries'))),
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class ReuseAbleRow extends StatefulWidget {
+  final String title, value;
+  const ReuseAbleRow({Key? key, required this.title, required this.value})
+      : super(key: key);
+
+  @override
+  State<ReuseAbleRow> createState() => _ReuseAbleRowState();
+}
+
+class _ReuseAbleRowState extends State<ReuseAbleRow> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(widget.title),
+          Text(widget.value),
+        ],
+      ),
+      const SizedBox(height: 5),
+      const Divider(),
+    ]);
   }
 }
