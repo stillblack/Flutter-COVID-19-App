1e6a1143df22913dba7a5b45534e9d84ec4ad561
StillBlack <stillblack2014@gmail.com>
2026-05-23 02:58:17 +0500

    pie chart  is added

--- lib/View/world_state.dart
+++ lib/View/world_state.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class WorldStateScreen extends StatefulWidget {
   const WorldStateScreen({Key? key}) : super(key: key);
@@ -7,9 +8,38 @@ class WorldStateScreen extends StatefulWidget {
   State<WorldStateScreen> createState() => _WorldStateScreenState();
 }
 
-class _WorldStateScreenState extends State<WorldStateScreen> {
+class _WorldStateScreenState extends State<WorldStateScreen>
+    with TickerProviderStateMixin {
+  late final _controller =
+      AnimationController(duration: const Duration(seconds: 3), vsync: this)
+        ..repeat();
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  final colorList = <Color>[
+    const Color(0xff4285f4), //
+    const Color(0xff1aa260), //
+    const Color(0xffde5246), //
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .01,
+          ),
+          PieChart(dataMap: const {
+            "Total": 20,
+            "Recoverd": 15,
+            "Deaths": 5,
+          }),
+        ]),
+      ),
+    );
   }
 }
